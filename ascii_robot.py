507aca4a75580282b9aa6fbe3725263aebecbba9
Oladapo01 <5giwao61@solent.ac.uk>
2021-10-01 23:00:27 +0100

    Initial commit

---
+++ ascii_robot.py
@@ -0,0 +1,7 @@
+#display a box
+print("Please enter a character for the eyes")
+character = input()
+print("##########")
+print(f"#  {character}  {character}  #")
+print("#  ----  #")
+print("##########")
