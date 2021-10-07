6c0b2f17cee4e10af6fa56cadb860a4c82982ad9
Oladapo01 <5giwao61@solent.ac.uk>
2021-10-07 10:00:59 +0100

    Initial commit

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+print("Towards what direction are you painting?")
+direction=input()
+if direction == "up":
+    print("I am painting in an upwards direction!")
+elif direction == "down":
+    print("I am painting in a downward direction!")
+elif direction == "right":
+    print("I am painting in a towards right!")
+elif direction == "left":
+    print("I am painting towards the left!")
+else:
+    print("Wrong direction!")
--- if_else.py
+++ if_else.py
@@ -1,15 +1,9 @@
-# check whether first number is bigger than the second number.
-# Assume first_number and second_number are existing variables.
-if 5 > 3:
-    print("First is bigger!")
-else:
-    print("First is equal or smaller!")
 
-print("Done!")
 
 #Enter the activity
 print("Please enter the activity to be performed")
 activity= input()
+#If activity is calculate
 if activity == "calculate":
     print("Performing calculations.....")
 else:
