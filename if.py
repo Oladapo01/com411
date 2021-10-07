851d8d2b65b871160182a21eb87e77d0ac282efa
Oladapo01 <5giwao61@solent.ac.uk>
2021-10-07 09:42:24 +0100

    Initial commit

---
+++ if.py
@@ -0,0 +1,7 @@
+#Ask of the book
+print("What type of book is this?")
+book = input()
+if book == "adventure":
+    print("I like adventure!")
+
+print("Finished reading book")
---
+++ if_else.py
@@ -0,0 +1,17 @@
+# check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+if 5 > 3:
+    print("First is bigger!")
+else:
+    print("First is equal or smaller!")
+
+print("Done!")
+
+#Enter the activity
+print("Please enter the activity to be performed")
+activity= input()
+if activity == "calculate":
+    print("Performing calculations.....")
+else:
+    print("Why?")
+print("Activity completed!")
