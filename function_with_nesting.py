43f9741d7e7b91ad4600a735beec0a81d2350748
Oladapo01 <5giwao61@solent.ac.uk>
2021-10-14 10:32:07 +0100

    Initial commit

---
+++ function_with_nesting.py
@@ -0,0 +1,11 @@
+def sight():
+    print("What lies before us?")
+    size= input()
+    if size == "a large boulder":
+        print("A large boulder")
+        print("It\'s time to run!")
+    elif size == "a small boulder":
+        print("We will be fine")
+    else:
+        print("Error")
+sight()
