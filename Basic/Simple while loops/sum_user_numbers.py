636a11fe05fa2825b1f9042812777def796d30f1
Oladapo01 <5giwao61@solent.ac.uk>
2021-11-17 15:34:30 +0000

    Initial commit

--- Basic/Simple while loops/sum_user_numbers.py
+++ Basic/Simple while loops/sum_user_numbers.py
@@ -1,7 +1,21 @@
+# Display message
 print("How many numbers should I sum up?")
 
+# input the amount of numbers to call
 number = int(input())
 
+# variable for counting
 sum_number = 0
 
-while
+# sum numbers
+summed_number = 0
+
+
+while sum_number < number:
+    print(f"please enter number {sum_number} of {number}")
+    input_number = int(input())
+    summed_number = summed_number + input_number
+    sum_number = sum_number + 1
+
+# display final result
+print(f"The total is {summed_number}")
